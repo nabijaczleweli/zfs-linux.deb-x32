c03f6b4778b324db7e173ec34aa15cae67b2e40a
наб <nabijaczleweli@gmail.com>
2020-05-22 17:46:17 +0200

    Initial README, with 0001-original-patches, 0002-logs, and 0003-clean-patches

---
+++ 0003-clean-patches/time.c
@@ -0,0 +1,8 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdint.h>
+int main() {
+        printf("time_t:    %d\n", sizeof(time_t));
+        printf("long:      %d\n", sizeof(long));
+        printf("long long: %d\n", sizeof(long long));
+}
